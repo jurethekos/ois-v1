4543fbe3f3c408434262565baa2de182954c9722
jurethekos <jurac.kos@gmail.com>
2020-03-20 12:37:23 +0000

    Dodajanje pospeska #2

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class Gravitacija{
 
     public static void main(String[] args) {
         
+        Scanner a = new Scanner(System.in); //prebere iz ukazne vrstice
+        
         System.out.println("OIS ni zakon!");
+        
+        int visina = a.nextInt();
+        System.out.println("9,81m/s2");
         
     }
 }
