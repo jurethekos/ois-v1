dc274ccdb3d3984979c1b94658fa72bf86b8193f
jurethekos <jurac.kos@gmail.com>
2020-03-20 11:31:07 +0000

    Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,8 @@
 public class Gravitacija{
 
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+        
+        System.out.println("OIS ni zakon!");
+        
     }
 }
